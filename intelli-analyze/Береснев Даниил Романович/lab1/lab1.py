bc9d0c85d21d44f97ad7899d87660d8ba621c821
beresven <beresven@gmail.com>
2022-04-06 19:17:01 +0300

    [1] Переработано 1.3

--- intelli-analyze/Береснев Даниил Романович/lab1/lab1.py
+++ intelli-analyze/Береснев Даниил Романович/lab1/lab1.py
@@ -9,65 +9,60 @@ all_percent = data["Cocoa Percent"].dropna()
 df = pd.concat([all_ref, all_rating], axis=1)
 
 f_ref = []
-conv_perc = []
-conv_perc_gb = []
+convert_perc = []
 
 for i in all_ref:
-	f_ref.append(str(i)[0])
+    f_ref.append(str(i)[0])
 
 df['fREF'] = f_ref
 df = df.sort_values(by='fREF').groupby('fREF')
 
 for i in all_percent:
-	conv_perc.append(float(i[:len(i)-1]))
+    convert_perc.append(float(i.rstrip('%')))
 
-for i in all_percent:
-	conv_perc_gb.append(round(float(i[:len(i)-1])/10)*10)
+convert_df_cocoa = pd.DataFrame(convert_perc, columns=["Cocoa Percent"])
 
-convert_df_cocoa = pd.DataFrame(conv_perc,columns=["Cocoa Percent"])
-df_cocoa = pd.concat([all_rating,convert_df_cocoa],axis = 1).sort_values(by="Cocoa Percent")
-
-convert_df_cocoa_gb = pd.DataFrame(conv_perc_gb,columns=['gb_perc'])
-df_cocoa_gb = pd.concat([all_rating,convert_df_cocoa_gb],axis = 1).sort_values(by = 'gb_perc').groupby('gb_perc')
+df_cocoa = pd.concat([all_rating, convert_df_cocoa], axis=1).sort_values(
+    by="Cocoa Percent").groupby('Cocoa Percent')
 
 plt.figure(1)
 
-plt.plot(df.mean()['Rating'],label='Усредненный график')
-plt.axhline(df.mean()['Rating'].median(),linestyle=':',color='b',label='Медиана по средним')
-plt.axhline(all_rating.median(),linestyle='-.',color='b',label='Медиана по всем')
-plt.axhline(all_rating.mean(),linestyle='-.',color='r',label='Среднее по всем')
+plt.plot(df.mean()['Rating'], label='Усредненный график')
+plt.axhline(df.mean()['Rating'].median(), linestyle=':',
+            color='b', label='Медиана по средним')
+plt.axhline(all_rating.median(), linestyle='-.',
+            color='b', label='Медиана по всем')
+plt.axhline(all_rating.mean(), linestyle='-.',
+            color='r', label='Среднее по всем')
 
-columns = ("По всем","По средним")
-rows = ("Дисперсия","СКО")
-n_rows = 10
-cell_text = [1,1]
+table_dt = {'По всем': [round(all_rating.var(), 2), round(all_rating.std(), 2)],
+            'По средним': [round(df.mean()['Rating'].var(), 2), round(df.mean()['Rating'].std(), 2)]}
 
-dt_raw = {'По всем'  :[round(all_rating.var(),2),round(all_rating.std(),2)],
-		  'По средним':[round(df.mean()['Rating'].var(),2),round(df.mean()['Rating'].std(),2)]}
-
-dt_DF = pd.DataFrame(data=dt_raw,index = ["Дисперсия","СКО"])
-plt.table(cellText=dt_DF.values,
-		  colWidths = [0.3, 0.3],
-          rowLabels=rows,
-          colLabels=columns,
-          rowColours=["lightgray"]*2,
-          colColours=["lightgray"]*2,
-          bbox = [0.2,-0.3,0.6,0.2],
+table_df = pd.DataFrame(data=table_dt, index=["Дисперсия", "СКО"])
+plt.table(cellText=table_df.values,
+          colWidths=[0.3, 0.3],
+          rowLabels=table_df.index,
+          colLabels=list(table_dt.keys()),
+          rowColours=["lightgray"] * 2,
+          colColours=["lightgray"] * 2,
+          bbox=[0.2, -0.3, 0.6, 0.2],
           cellLoc='center')
 
-plt.subplots_adjust(left=0.2, bottom=0.25)
+plt.subplots_adjust(bottom=0.25)
 plt.legend()
 
-plt.figure(2)
-shift=0.05
+plt.figure(2, figsize=[13, 4.8])
 
-plt.plot(df_cocoa['Cocoa Percent'],df_cocoa['Rating'])
-for i in df_cocoa_gb:
-	print(i[1])
-	plt.axhline(i[1]['Rating'].max(),linestyle='-',color='lightgreen',label='max',xmin=shift-0.04,xmax=shift+0.04)
-	plt.axhline(i[1]['Rating'].min(),linestyle='-',color='r',label='min',xmin=shift-0.04,xmax=shift+0.04)
-	plt.axhline(i[1]['Rating'].var(),linestyle='-.',color='black',label='var',xmin=shift-0.04,xmax=shift+0.04)
-	shift+=0.15
+plt.plot(df_cocoa.mean())
+plt.plot(df_cocoa['Rating'].max(), color='lightgreen')
+plt.plot(df_cocoa['Rating'].min(), color='r')
+plt.plot(df_cocoa['Rating'].var(), color='black')
 
-plt.legend(['Rating','Max','Min','Var'])
+perc_xticks = list(df_cocoa['Cocoa Percent'].groups.keys())
+
+plt.xticks(perc_xticks, [str(x).rstrip('0').rstrip('.')
+                         for x in perc_xticks], rotation=70, size=9)
+
+plt.legend(['Mean rating', 'Max', 'Min', 'Var'])
+plt.subplots_adjust(right=0.98, left=0.02)
 plt.show()
