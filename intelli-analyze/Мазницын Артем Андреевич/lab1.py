949ba8f3b69313308d2a34cdaac65550b114ccde
Артём Мазницын <64485156+RaRaze96@users.noreply.github.com>
2022-04-07 23:44:00 +0300

    заготовка данных

--- intelli-analyze/Мазницын Артем Андреевич/lab1.py
+++ intelli-analyze/Мазницын Артем Андреевич/lab1.py
@@ -1,1 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
+data = pd.read_csv('./flavors_of_cacao.csv', header=0)
+
+rating = data['Rating']
+ref = data[['REF', 'Rating']]
+
+def lowNums(x):
+  while len(str(x['REF'].max())) >= 2:
+        x.loc[x['REF'] > 9, 'REF'] //= 10
+    return 1
+
+  
+lowNums(ref)
+ref = ref.groupby(by="REF")[['REF', 'Rating']]
