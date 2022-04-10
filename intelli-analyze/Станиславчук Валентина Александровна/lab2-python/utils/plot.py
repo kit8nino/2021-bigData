bf858aba4801d9e485bcb417bb687c554d2c7b29
valentinastanislavchuk <valentina.stanislavchuk@yandex.ru>
2022-04-11 02:19:06 +0300

    intelli-analyze lab_1 & lab_2 refactored

---
+++ intelli-analyze/Станиславчук Валентина Александровна/lab2-python/utils/plot.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+def __get_ylim(ser):
+  if type(ser) is list:
+    ser = pd.concat(ser)
+    
+  return ser.max() + ser.max() * 0.1
+
+def plot_bars(ser, title, xlabel, ylabel, **kwargs):
+  if type(ser) is list:
+    fig, axes = plt.subplots(nrows=len(ser), figsize=(len(ser[0]) * 1.3, len(ser) * 4))
+    fig.subplots_adjust(hspace=0.3)
+    axes[0].set_title(title + '\n', fontsize=14)
+
+    for ax, idx in zip(axes, range(len(axes))):
+      ax.set(xlabel=xlabel, ylabel=ylabel, ylim=[0, __get_ylim(ser)])
+      ax.bar_label(ax.bar(ser[idx].index, ser[idx].round(2)))
+
+  else:
+    fig, ax = plt.subplots(nrows=1, figsize=(len(ser) * 1.3, 4))
+    ax.set_title(title + '\n', fontsize=14)
+    
+    ax.set(xlabel=xlabel, ylabel=ylabel, ylim=[0, __get_ylim(ser)])
+    ax.bar_label(ax.bar(ser.index, ser.round(2)))
