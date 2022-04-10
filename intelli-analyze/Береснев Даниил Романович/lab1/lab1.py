39ba4daeaead54efb5fa65e860cc48f5156a733e
beresven <beresven@gmail.com>
2022-04-10 21:17:27 +0300

    [1] Фиксы 1.2

--- intelli-analyze/Береснев Даниил Романович/lab1/lab1.py
+++ intelli-analyze/Береснев Даниил Романович/lab1/lab1.py
@@ -25,31 +25,29 @@ convert_df_cocoa = pd.DataFrame(convert_perc, columns=["Cocoa Percent"])
 df_cocoa = pd.concat([all_rating, convert_df_cocoa], axis=1).sort_values(
     by="Cocoa Percent").groupby('Cocoa Percent')
 
-plt.figure(1)
+plt.figure(1, figsize=[7.5, 5])
 
-plt.plot(df.mean()['Rating'], label='Усредненный график')
-plt.axhline(df.mean()['Rating'].median(), linestyle=':',
-            color='b', label='Медиана по средним')
+plt.subplot(212)
+plt.plot(df['Rating'].mean(), label='Среднее')
+plt.plot(df['Rating'].median(), linestyle=':', color='b', label='Медианное')
 plt.axhline(all_rating.median(), linestyle='-.',
             color='b', label='Медиана по всем')
 plt.axhline(all_rating.mean(), linestyle='-.',
             color='r', label='Среднее по всем')
-
-table_dt = {'По всем': [round(all_rating.var(), 2), round(all_rating.std(), 2)],
-            'По средним': [round(df.mean()['Rating'].var(), 2), round(df.mean()['Rating'].std(), 2)]}
-
-table_df = pd.DataFrame(data=table_dt, index=["Дисперсия", "СКО"])
-plt.table(cellText=table_df.values,
-          colWidths=[0.3, 0.3],
-          rowLabels=table_df.index,
-          colLabels=list(table_dt.keys()),
-          rowColours=["lightgray"] * 2,
-          colColours=["lightgray"] * 2,
-          bbox=[0.2, -0.3, 0.6, 0.2],
-          cellLoc='center')
-
-plt.subplots_adjust(bottom=0.25)
 plt.legend()
+
+plt.subplot(222)
+plt.title("Дисперсия")
+plt.plot(df['Rating'].var(), linestyle='-', color='black', label='Дисперсия')
+plt.axhline(all_rating.var(), linestyle=':',
+            color='black', label='Дисперсия по всем')
+
+plt.subplot(221)
+plt.title("CKO")
+plt.plot(df['Rating'].std(), linestyle='-', color='b')
+plt.axhline(all_rating.std(), linestyle=':', color='b')
+
+plt.subplots_adjust(bottom=0.15, right=0.98, left=0.07, top=0.90)
 
 plt.figure(2, figsize=[13, 4.8])
 
