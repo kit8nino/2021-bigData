a2cde8b8a022ddf7c6cfbf2ac8563dea3c18eead
valentinastanislavchuk <valentina.stanislavchuk@yandex.ru>
2022-04-11 03:09:20 +0300

    intelli-analyze lab_1 & lab_2 refactored 4

--- intelli-analyze/Станиславчук Валентина Александровна/lab1-python/utils/plot.py
+++ intelli-analyze/Станиславчук Валентина Александровна/lab1-python/utils/plot.py
@@ -7,19 +7,27 @@ def __get_ylim(ser):
     
   return ser.max() + ser.max() * 0.1
 
-def plot_bars(ser, title, xlabel, ylabel, **kwargs):
-  if type(ser) is list:
-    fig, axes = plt.subplots(nrows=len(ser), figsize=(len(ser[0]) * 0.8, len(ser) * 4))
-    fig.subplots_adjust(hspace=0.3)
-    axes[0].set_title(title + '\n', fontsize=14)
 
-    for ax, idx in zip(axes, range(len(axes))):
-      ax.set(xlabel=xlabel, ylabel=ylabel, ylim=[0, __get_ylim(ser)])
-      ax.bar_label(ax.bar(ser[idx].index, ser[idx].round(2)))
+def multiple_plot_bars(ser, title, xlabel, ylabel):
+  fig, axes = plt.subplots(nrows=len(ser), figsize=(len(ser[0]) * 1.3, len(ser) * 4))
+  fig.subplots_adjust(hspace=0.3)
+  axes[0].set_title(title + '\n', fontsize=14)
 
-  else:
-    fig, ax = plt.subplots(nrows=1, figsize=(len(ser) * 0.8, 4))
-    ax.set_title(title + '\n', fontsize=14)
-    
+  for ax, idx in zip(axes, range(len(axes))):
     ax.set(xlabel=xlabel, ylabel=ylabel, ylim=[0, __get_ylim(ser)])
-    ax.bar_label(ax.bar(ser.index, ser.round(2)))
+    ax.bar_label(ax.bar(ser[idx].index, ser[idx].round(2)))
+
+
+def single_plot_bars(ser, title, xlabel, ylabel):
+  fig, ax = plt.subplots(nrows=1, figsize=(len(ser) * 1.3, 4))
+  ax.set_title(title + '\n', fontsize=14)
+  
+  ax.set(xlabel=xlabel, ylabel=ylabel, ylim=[0, __get_ylim(ser)])
+  ax.bar_label(ax.bar(ser.index, ser.round(2)))
+
+
+def plot_bars(ser, title, xlabel, ylabel):
+  if type(ser) is list:
+    multiple_plot_bars(ser, title, xlabel, ylabel)
+  else:
+    single_plot_bars(ser, title, xlabel, ylabel)
