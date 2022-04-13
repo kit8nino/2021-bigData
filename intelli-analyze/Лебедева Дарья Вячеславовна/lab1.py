7a09a84b14ddaa782a1e98b3cdb57963359d0f42
Doshik03 <lebedeva.dasha2001@yandex.ru>
2022-04-13 11:11:40 +0300

    First part of task

--- intelli-analyze/Лебедева Дарья Вячеславовна/lab1.py
+++ intelli-analyze/Лебедева Дарья Вячеславовна/lab1.py
@@ -5,3 +5,6 @@ import matplotlib.pyplot as plt
 data = pd.read_csv('./flavors_of_cacao.csv', header=0)
 data['Cocoa Percent'] = data['Cocoa Percent'].replace('%', '', regex=True)
 rating = data['Rating']
+
+reviewDateGroup = data[['Review Date', 'Rating']]
+reviewDateGroup = reviewDateGroup.groupby('Review Date')[['Review Date', 'Rating']]
