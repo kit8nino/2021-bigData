294621866e61e15f12f373312aca29274413b5f7
NTalexGHOST <alexpaa21@gmail.com>
2022-05-11 23:50:54 +0300

    Do a third task and fix some things

--- intelli-analyze/Павлов Алексей Андреевич/lab2.py
+++ intelli-analyze/Павлов Алексей Андреевич/lab2.py
@@ -7,10 +7,10 @@ data['Cocoa Percent'] = data['Cocoa Percent'].replace('%', '', regex=True).astyp
 
 
 def possibilities(info, condition):
-    df = info.query(condition)
+    dataframe = info.query(condition)
     info = info.groupby('Company Location')
-    df = df.groupby('Company Location')
-    return df['Rating'].count() / info['Rating'].count()
+    dataframe = dataframe.groupby('Company Location')
+    return dataframe['Rating'].count() / info['Rating'].count()
 
 
 # Посчитать априорные вероятности для каждой страны происхождения (Company Location) получения оценки выше 3.1
@@ -22,14 +22,22 @@ print(temp)
 # Используя их, посчитать вероятность того, что новый сорт какао с содержанием выше 73% (Cocoa Percent)
 # будет иметь оценку выше 3.1 для стран b) северного полушария
 
-northernCountries = ['Amsterdam', 'Belgium', 'Brazil', 'Canada', 'Colombia', 'Ecuador', 'France', 'Israel', 'Italy', 'Lithuania', 'Sao Tome', 'Scotland', 'Switzerland', 'U.K.', 'U.S.A.']
+northernCountries = ['Amsterdam', 'Belgium', 'Brazil', 'Canada', 'Colombia', 'Ecuador', 'France', 'Israel', 'Italy',
+                     'Lithuania', 'Sao Tome', 'Scotland', 'Switzerland', 'U.K.', 'U.S.A.']
 temp = possibilities(data, 'Rating > 3.1 and `Cocoa Percent` > 73') / possibilities(data, '`Cocoa Percent` > 73')
 temp = temp[northernCountries]
 plt.figure(figsize=(16, 7))
+plt.title('№2 Вероятность того, что новый сорт с содержанием какао выше 73% будет иметь оценку'
+          ' выше 3.1 для стран северного полушария')
 plt.bar(temp.index, temp.array)
 plt.show()
 
 # Сделать прогноз, какова вероятность того, что обзоры какао после 2014 года будут иметь оценку выше медианной
 # по всему периоду после 2010 года.
 
-
+df = data.query('`Review Date` > 2014')
+medianValue = data.query('`Review Date` > 2010')['Rating'].median()
+temp = df.query(f'Rating > {medianValue}')['Rating'].count() / df['Rating'].count()
+print('----- №3 Прогноз вероятности, что обзоры после 2014 года будут иметь оценку'
+      ' выше медианной по всему периоду 2010 года -----')
+print(temp)
