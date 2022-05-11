07500a4292daa929b39b413f37218407ac92b5b9
NTalexGHOST <alexpaa21@gmail.com>
2022-05-11 23:28:52 +0300

    Remove unnecessary code from prev. lab and do a first task

--- intelli-analyze/Павлов Алексей Андреевич/lab2.py
+++ intelli-analyze/Павлов Алексей Андреевич/lab2.py
@@ -3,80 +3,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 data = pd.read_csv('./flavors_of_cacao.csv', header=0)
-rating = data['Rating']
 data['Cocoa Percent'] = data['Cocoa Percent'].replace('%', '', regex=True)
-cocoaColumn = data.sort_values(by='Cocoa Percent')['Cocoa Percent'].unique()
 
-cocoaColumn = cocoaColumn.astype(float)
-cocoaColumn = np.sort(cocoaColumn)
-cocoaColumn = cocoaColumn.astype(str)
-cocoaColumn = [i.replace('.0', '') for i in cocoaColumn]
-cocoaGroup = data.groupby('Cocoa Percent')['Rating']
 
-# Первый пункт
+def possibilities(info, condition):
+    df = info.query(condition)
+    info = info.groupby('Company Location')
+    df = df.groupby('Company Location')
+    return df['Rating'].count() / info['Rating'].count()
 
-refGroup = data[['REF', 'Rating']]
-i = 0
-while i <= len(str(refGroup['REF'].max())):
-    i += 1
-    refGroup.loc[refGroup['REF'] > 9, 'REF'] //= 10
-refGroup = refGroup.groupby("REF")[['REF', 'Rating']]
 
-# Второй пункт
+# Посчитать априорные вероятности для каждой страны происхождения (Company Location) получения оценки выше 3.1
 
-plt.figure(figsize=(15, 7))
-сolumn = refGroup.groups.keys()
-tempArray = np.empty(9)
+temp = possibilities(data, 'Rating > 3.1').dropna()
+print('----- №1 Вероятность получения оценки выше 3.1 -----')
+print(temp)
 
-tempArray[...] = rating.var()
-plt.subplot(2, 2, 1)
-plt.plot(сolumn, refGroup.var().fillna(0)['Rating'], label='Дисперсия Rating')
-plt.plot(сolumn, tempArray, label='Общая дисперсия')
-plt.title("Дисперсия")
-plt.legend(fontsize=10)
+# Используя их, посчитать вероятность того, что новый сорт какао с содержанием выше 73% (Cocoa Percent)
+# будет иметь оценку выше 3.1 для стран b) северного полушария
 
-tempArray[...] = rating.mean()
-plt.subplot(2, 2, 2)
-plt.plot(сolumn, refGroup.mean()['Rating'], label='Среднее Rating')
-plt.plot(сolumn, tempArray, label='Общее среднее')
-plt.title("Среднее")
-plt.legend(fontsize=10)
 
-tempArray[...] = rating.median()
-plt.subplot(2, 2, 3)
-plt.plot(сolumn, refGroup.median()['Rating'], label='Медианное Rating')
-plt.plot(сolumn, tempArray, label='Общее Медианное')
-plt.title("Медианное")
-plt.legend(fontsize=10)
 
-tempArray[...] = rating.std()
-plt.subplot(2, 2, 4)
-plt.plot(сolumn, refGroup.std()['Rating'], label='СКО Rating')
-plt.plot(сolumn, tempArray, label='Общее СКО')
-plt.title("СКО")
-plt.legend(fontsize=10)
+# Сделать прогноз, какова вероятность того, что обзоры какао после 2014 года будут иметь оценку выше медианной
+# по всему периоду после 2010 года.
 
-plt.show()
 
-# Третий пункт
-
-plt.figure(figsize=(15, 7))
-tempArray = np.empty(len(cocoaColumn))
-
-tempArray[...] = cocoaGroup.var().fillna(0)
-plt.subplot(2, 1, 1)
-plt.plot(cocoaColumn, tempArray, label='Дисперсия')
-tempArray[...] = rating.var()
-plt.plot(cocoaColumn, tempArray, label='Общая дисперсия')
-plt.title("Дисперсия")
-plt.legend(fontsize=10)
-
-tempArray[...] = cocoaGroup.max() - cocoaGroup.min()
-plt.subplot(2, 1, 2)
-plt.plot(cocoaColumn, tempArray, label='Размах')
-tempArray[...] = rating.max() - rating.min()
-plt.plot(cocoaColumn, tempArray, label='Общий размах')
-plt.title("Размах")
-plt.legend(fontsize=10)
-
-plt.show()
