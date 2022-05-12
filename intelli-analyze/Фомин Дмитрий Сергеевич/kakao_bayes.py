36f46717258d168a65be032352c7b551f793211c
Dimasaurus <92543865+Dimasaurus@users.noreply.github.com>
2022-05-12 20:04:55 +0300

    Update kakao_bayes.py

--- intelli-analyze/Фомин Дмитрий Сергеевич/kakao_bayes.py
+++ intelli-analyze/Фомин Дмитрий Сергеевич/kakao_bayes.py
@@ -1,3 +1,4 @@
+
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import pandas as pd
@@ -20,18 +21,19 @@ dfper = pd.concat([loc, rat, per], axis=1)
 dfper1 = pd.concat([loc, rat, per], axis=1).groupby('Company Location').size()
 
 df2 = df.where(df['Rating']>3.1).groupby('Company Location').size()
-notdf22 = dfper.where((dfper['Rating']<3.1) & (dfper['Cocoa Percent']>73)).groupby('Company Location').size()
-df22 = dfper.where((dfper['Rating']>3.1) & (dfper['Cocoa Percent']>73)).groupby('Company Location').size()
+df73 = dfper.where(dfper['Cocoa Percent']>73).groupby('Company Location').size()
+notdf22 = dfper.where(dfper['Rating']<=3.1).groupby('Company Location').size()
+df22 = dfper.where(dfper['Rating']>3.1).groupby('Company Location').size()
 df3 = df2 / df1
-df33 = df22/ dfper1
-notdf33= notdf22 / dfper1 
-print(df3) 
+df33 = df22/ df73
+notdf33= notdf22 / df73 
+ 
 
-EU_AF = ['Amsterdam', 'Austria', 'Belgium', 'Czech Republic', 'Denmark', 'Finland', 'France', 'Germany', 'Ghana', 'Hungary', 'Iceland', 'Ireland', 'Italy', 'Lithuania', 'Poland', 'Portugal', 'Scotland', 'South Africa', 'Spain', 'Sweden', 'Switzerland', 'U.K.', 'Wales']
-
+EU_AF = ['Amsterdam', 'Austria', 'Belgium', 'Denmark', 'Finland', 'France', 'Germany', 'Ghana', 'Hungary', 'Iceland', 'Ireland', 'Italy', 'Lithuania', 'Poland', 'Portugal', 'Scotland', 'Spain', 'Sweden', 'Switzerland', 'U.K.', 'Wales']
+# 'South Africa', 'Czech Republic',
 df4 = df3.loc[EU_AF] #pa
 df5 = df33.loc[EU_AF] #pba
 df6 = notdf33.loc[EU_AF] #pbna
 pB = df5*df4 + df6*(1-df4)
-
+print(pB)
 print(bayes(df4,pB,df5))
