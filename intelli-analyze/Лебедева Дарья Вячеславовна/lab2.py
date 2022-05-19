aa45dc01c9b8224feb2cfd6914f8e5a2ce6330f6
Doshik03 <lebedeva.dasha2001@yandex.ru>
2022-05-19 13:45:04 +0300

    Do first and third part with some fixes

--- intelli-analyze/Лебедева Дарья Вячеславовна/lab2.py
+++ intelli-analyze/Лебедева Дарья Вячеславовна/lab2.py
@@ -1,73 +1,26 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('./flavors_of_cacao.csv', header=0)
-data['Cocoa Percent'] = data['Cocoa Percent'].replace('%', '', regex=True)
-rating = data['Rating']
+mainDF = pd.read_csv('./flavors_of_cacao.csv', header=0)
+mainDF['Cocoa Percent'] = mainDF['Cocoa Percent'].replace('%', '', regex=True).astype('float64')
 
-reviewDateGroup = data[['Review Date', 'Rating']]
-reviewDateGroup = reviewDateGroup.groupby('Review Date')[['Review Date', 'Rating']]
+tempDF1 = mainDF.query('Rating > 3.1')
+tempDF2 = mainDF.groupby('Company Location')
+tempDF1 = tempDF1.groupby('Company Location')
+tempDF = (tempDF1['Rating'].count() / tempDF2['Rating'].count()).dropna()
 
-plt.figure(figsize=(15, 7))
-сolumn = reviewDateGroup.groups.keys()
-array = np.empty(len(reviewDateGroup.groups.values()))
+print('[1] Вероятность получения оценки выше 3.1')
+print(tempDF)
 
-array[...] = rating.var()
-plt.subplot(2, 2, 1)
-plt.plot(сolumn, reviewDateGroup.var().fillna(0)['Rating'], label='Дисперсия Rating')
-plt.plot(сolumn, array, label='Общая дисперсия')
-plt.title("Дисперсия")
-plt.legend(fontsize=10)
 
-array[...] = rating.mean()
-plt.subplot(2, 2, 2)
-plt.plot(сolumn, reviewDateGroup.mean()['Rating'], label='Среднее Rating')
-plt.plot(сolumn, array, label='Общее среднее')
-plt.title("Среднее")
-plt.legend(fontsize=10)
+bothAmericas = ['Bolivia', 'Brazil', 'Canada', 'Colombia', 'Ecuador', 'U.S.A.']
 
-array[...] = rating.median()
-plt.subplot(2, 2, 3)
-plt.plot(сolumn, reviewDateGroup.median()['Rating'], label='Медианное Rating')
-plt.plot(сolumn, array, label='Общее Медианное')
-plt.title("Медианное")
-plt.legend(fontsize=10)
 
-array[...] = rating.std()
-plt.subplot(2, 2, 4)
-plt.plot(сolumn, reviewDateGroup.std()['Rating'], label='СКО Rating')
-plt.plot(сolumn, array, label='Общее СКО')
-plt.title("СКО")
-plt.legend(fontsize=10)
 
-plt.show()
 
-#---------------------------------------------------------------------------
-
-cocoaColumn = data.sort_values(by='Cocoa Percent')['Cocoa Percent'].unique()
-cocoaColumn = cocoaColumn.astype(float)
-cocoaColumn = np.sort(cocoaColumn)
-cocoaColumn = cocoaColumn.astype(str)
-cocoaColumn = [i.replace('.0', '') for i in cocoaColumn]
-cocoaGroup = data.groupby('Cocoa Percent')['Rating']
-
-plt.figure(figsize=(15, 7))
-thirdArray = np.empty(len(cocoaColumn))
-thirdArray[...] = cocoaGroup.var().fillna(0)
-plt.subplot(2, 1, 1)
-plt.plot(cocoaColumn, thirdArray, label='Дисперсия')
-thirdArray[...] = rating.var()
-plt.plot(cocoaColumn, thirdArray, label='Общая дисперсия')
-plt.title("Дисперсия")
-plt.legend(fontsize=10)
-
-thirdArray[...] = cocoaGroup.max() - cocoaGroup.min()
-plt.subplot(2, 1, 2)
-plt.plot(cocoaColumn, thirdArray, label='Размах')
-thirdArray[...] = rating.max() - rating.min()
-plt.plot(cocoaColumn, thirdArray, label='Общий размах')
-plt.title("Размах")
-plt.legend(fontsize=10)
-
-plt.show()
+tempDF = mainDF.query('`Review Date` > 2014')
+medianValue = mainDF.query('`Review Date` > 2010')['Rating'].median()
+tempDF = tempDF.query(f'Rating > {medianValue}')['Rating'].count() / tempDF['Rating'].count()
+print('[3] Прогноз вероятности, что обзоры после 2014 года будут иметь оценку'
+      ' выше медианной по всему периоду 2010 года')
+print(tempDF)
