7f2384c98f73cac7a20713b06a3651b9cd50944e
Dimasaurus <92543865+Dimasaurus@users.noreply.github.com>
2022-05-26 18:29:54 +0300

    Update kakao_bayes.py

--- intelli-analyze/Фомин Дмитрий Сергеевич/kakao_bayes.py
+++ intelli-analyze/Фомин Дмитрий Сергеевич/kakao_bayes.py
@@ -20,20 +20,18 @@ df1 = pd.concat([loc, rat], axis=1).groupby('Company Location').size()
 dfper = pd.concat([loc, rat, per], axis=1)
 dfper1 = pd.concat([loc, rat, per], axis=1).groupby('Company Location').size()
 
-df2 = df.where(df['Rating']>3.1).groupby('Company Location').size()
-df73 = dfper.where(dfper['Cocoa Percent']>73).groupby('Company Location').size()
-notdf22 = dfper.where(dfper['Rating']<=3.1).groupby('Company Location').size()
-df22 = dfper.where(dfper['Rating']>3.1).groupby('Company Location').size()
-df3 = df2 / df1
-df33 = df22/ df73
-notdf33= notdf22 / df73 
+df_rating = df.where(df['Rating']>3.1).groupby('Company Location').size()
+df_perc = dfper.where(dfper['Cocoa Percent']>73).groupby('Company Location').size()
+dfBoth = dfper.where((dfper['Cocoa Percent']>73)&(dfper['Rating']>3.1)).groupby('Company Location').size()
+df_rating_prob = df_rating / df1
+df_perc_prob =df_perc / df1
+dfBoth_prob= dfBoth / df1 
  
 
-EU_AF = ['Amsterdam', 'Austria', 'Belgium', 'Denmark', 'Finland', 'France', 'Germany', 'Ghana', 'Hungary', 'Iceland', 'Ireland', 'Italy', 'Lithuania', 'Poland', 'Portugal', 'Scotland', 'Spain', 'Sweden', 'Switzerland', 'U.K.', 'Wales']
-# 'South Africa', 'Czech Republic',
-df4 = df3.loc[EU_AF] #pa
-df5 = df33.loc[EU_AF] #pba
-df6 = notdf33.loc[EU_AF] #pbna
-pB = df5*df4 + df6*(1-df4)
-print(pB)
-print(bayes(df4,pB,df5))
+EU_AF = ['Amsterdam', 'Austria', 'Belgium','Czech Republic', 'Denmark', 'Finland', 'France', 'Germany', 'Ghana', 'Hungary', 'Iceland', 'Ireland', 'Italy', 'Lithuania', 'Poland', 'Portugal','South Africa', 'Scotland', 'Spain', 'Sweden', 'Switzerland', 'U.K.', 'Wales']
+ 
+pA = df_rating_prob.loc[EU_AF] 
+pB = df_perc_prob.loc[EU_AF] 
+pBA = dfBoth_prob.loc[EU_AF] 
+
+print(bayes(pA,pB,pBA))
