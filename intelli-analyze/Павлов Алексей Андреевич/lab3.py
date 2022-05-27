6b751f4703ba6d11c71da163b0f95d2f5b77b0bb
Doshik03 <alexpaa21@gmail.com>
2022-05-27 15:16:58 +0300

    Fix lab

--- intelli-analyze/Павлов Алексей Андреевич/lab3.py
+++ intelli-analyze/Павлов Алексей Андреевич/lab3.py
@@ -1,29 +1,20 @@
 import math
 
 
-def calculate(x):
-    return x - (2 * math.sin(x) - math.atan(x))
+def func(x):
+    return x + (2 / (a + b)) * (2 * math.sin(x) - math.atan(x))
 
 
-def solve_equation(a, b, e):
-    curIter = 1
-    x = (a + b) / 2
-    y = calculate(x)
-    while abs(y - x) > e:
-        x = y
-        y = calculate(x)
-        curIter += 1
-    if a < x < b:
-        return f"{curIter} step: X = {x}, Y = {2 * math.sin(x) - math.atan(x)}"
+def calculate(x, e):
+    if abs(func(x) - x) > e:
+        return calculate(func(x), e)
     else:
-        print('The program cannot calculate Xs with a given accuracy in a given range')
-        print('However, X was found in a another range')
-        return f"{curIter} step: X = {x}, Y = {2 * math.sin(x) - math.atan(x)}"
+        return func(x)
 
 
 print("\nEquation: 2 * sinX - arctgX = 0")
-entry = 2.5
-end = 2.6
+a = 2.5
+b = 2.6
 accuracy = float(input("Accuracy of the equation?\n"))
 
-print(solve_equation(entry, end, accuracy))
+print("x = ", calculate((a + b) / 2, accuracy))
